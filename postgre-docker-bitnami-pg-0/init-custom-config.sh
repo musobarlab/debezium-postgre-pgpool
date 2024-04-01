4eca3143777e0c526a75090268b83cbe539f5c9c
wuriyanto <wuriyanto48@yahoo.co.id>
2024-04-02 04:10:13 +0700

    fix initd location

--- postgre-docker-bitnami-pg-0/init-custom-config.sh
+++ postgre-docker-bitnami-pg-0/init-custom-config.sh
@@ -1,5 +1,5 @@
 #!/bin/bash
 set -e
 
-cat /tmp/postgresql.conf.1.sample > /bitnami/postgresql/conf/postgresql.conf
-cat /tmp/pg_hba.1.conf.sample > /bitnami/postgresql/confpg_hba.conf
+cat /tmp/postgresql.conf.1.sample > /opt/bitnami/postgresql/conf/postgresql.conf
+cat /tmp/pg_hba.1.conf.sample > /opt/bitnami/postgresql/conf/pg_hba.conf
