2c2854010d66aa80dcafd89f0cfd768500c35444
wuriyanto <wuriyanto48@yahoo.co.id>
2024-04-02 11:54:59 +0700

    fixing error replication role creation error

--- postgre-docker-bitnami-pg-0/init-custom-config.sh
+++ postgre-docker-bitnami-pg-0/init-custom-config.sh
@@ -3,3 +3,7 @@ set -e
 
 cat /tmp/postgresql.conf.1.sample > /opt/bitnami/postgresql/conf/postgresql.conf
 cat /tmp/pg_hba.1.conf.sample > /opt/bitnami/postgresql/conf/pg_hba.conf
+
+# warning: danger
+export PGPASSWORD=adminpassword
+psql -U postgres -c 'ALTER USER codebaseuser REPLICATION;'
